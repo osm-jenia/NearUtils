fe3e9d19dff9d964ad28fbb7e0bdb06cf00477ec
osm-jenia <osm-jenia@ya.ru>
2021-09-12 14:56:42 +0300

    Изменил клавишу вызоыва Alt-Shift-3

--- NearUtils/src/main/java/ru/ojaqua/NearUtils/App.java
+++ NearUtils/src/main/java/ru/ojaqua/NearUtils/App.java
@@ -82,7 +82,8 @@ public class App
             	
             	var systemTray = new USystemTray(SystemTray.getSystemTray(), menu);
             	
-            	JIntellitype.getInstance().registerHotKey(WINDOWS_A, JIntellitype.MOD_WIN, 'A');
+            	//JIntellitype.getInstance().registerHotKey(WINDOWS_A, JIntellitype.MOD_WIN, 'A');
+            	JIntellitype.getInstance().registerHotKey(WINDOWS_A, JIntellitype.MOD_ALT + JIntellitype.MOD_SHIFT, 'B');
             	JIntellitype.getInstance().addHotKeyListener(aIdentifier->{if(aIdentifier == WINDOWS_A)
             		                                                         menu.show();
             		                                                        
